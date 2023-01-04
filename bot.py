da4c0d263ba0f579429a4400e6312a2e29aa0d2f
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-01-04 18:55:25 +0100

    Try something

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
 import discord
 
-# Yooho un commentaire
+client = discord.Client()
+client.run('MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
