4bcb59a21ad8d5c690c1f402d30756ce5b0f89a8
Gylfirst <matt.tourrette@hotmail.fr>
2023-01-04 20:40:37 +0100

    fixes

--- bot.py
+++ bot.py
@@ -1,25 +1,23 @@
-from discord import *
+import discord
+import discord.ext
 
-intents = Intents.default()
-intents.message_content = True
+# setting up the bot
+intents = discord.Intents.all() 
+# if you don't want all intents you can do discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
-client = Client(intents=intents)
-
+# sync the slash command to your server
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    await tree.sync(guild=discord.Object(id=284980887634837504)) #guild id = server id
+    # print "ready" in the console when the bot is ready to work
+    print("Ready")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# make the slash command
+@tree.command(name="ping", description="my ping")
+async def slashing_commanding(int: discord.Interaction):    
+    await int.response.send_message("Pong!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-#fixing
-@client.event
-async def ping(ctx):
-    await ctx.reply("Pong !")
-
-client.run('MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig')
+# run the bot
+client.run("MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig")
