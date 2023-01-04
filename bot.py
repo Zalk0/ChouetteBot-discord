e89e1d5eca4c2534c32f5822af6fd86c1969de1a
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-01-04 22:48:16 +0100

    Modify indentation

--- bot.py
+++ bot.py
@@ -4,27 +4,27 @@ import discord
 intents = discord.Intents.all()
 
 class aclient(discord.Client):
-  def __init__(self):
-    super().__init__(intents = intents)
-    self.synced = False
-    self.added = False
+	def __init__(self):
+		super().__init__(intents = intents)
+		self.synced = False
+		self.added = False
 
-  async def on_ready(self):
-    await self.wait_until_ready()
-    if not self.synced:
-        await tree.sync()
-        self.synced = True
-    if not self.added:
-      self.added = True
-    print(f"Logged in {self.user}!")
+	async def on_ready(self):
+		await self.wait_until_ready()
+		if not self.synced:
+			await tree.sync()
+			self.synced = True
+		if not self.added:
+			self.added = True
+		print(f"Logged in {self.user}!")
 
 client = aclient()
 tree = discord.app_commands.CommandTree(client)
 
-# make the slash command
+# make the ping command
 @tree.command(name="ping", description="You can test your ping here!")
-async def slashing_commanding(int: discord.Interaction):    
-    await int.response.send_message("Pong!")
+async def slashing_commanding(int: discord.Interaction):
+	await int.response.send_message("Pong!")
 
 # run the bot
 client.run("MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig")
