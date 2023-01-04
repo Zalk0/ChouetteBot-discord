8b384d489ede881b04369e389255330ad5402b7a
Gylfirst <matt.tourrette@hotmail.fr>
2023-01-04 19:43:51 +0100

    test

--- bot.py
+++ bot.py
@@ -1,8 +1,25 @@
-import discord
+from discord import *
 
-client = discord.Client()
-client.run('MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig')
+intents = Intents.default()
+intents.message_content = True
 
-@client.command()
+client = Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+#fixing
+@client.event
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.reply("Pong !")
+
+client.run('MTA2MDI1MjY1MjI4MzM3OTg5NA.GIWIoq.U6SL91qCbXhsK06YnWoK30g9UP2ql881bRpVig')
