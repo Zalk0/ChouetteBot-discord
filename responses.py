44ea78a3467545c05f1edf562ef274d2ca0ef92c
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-01-13 17:54:25 +0100

    Forgot return

--- responses.py
+++ responses.py
@@ -3,3 +3,6 @@ def responses(message: str) -> str:
     # Checks if a message ends with quoi
     if ''.join(filter(str.isalpha, message)).lower().endswith("quoi"):
         return "**FEUR**"
+
+    # Return empty string if no condition is checked
+    return ''
