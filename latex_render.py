6a6954c32f61fdfc6f4805d166a515378e4bf1bc
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-03-07 10:44:41 +0100

    Update bg color for LaTeX

--- latex_render.py
+++ latex_render.py
@@ -7,7 +7,7 @@ import requests
 # Make a LaTeX rendering function using an online equation renderer : https://latex.codecogs.com/
 # Then send the image as a file
 async def latex_render(equation: str) -> discord.File:
-    options = r"\bg_black \color[RGB]{240, 240, 240} \pagecolor[RGB]{54, 57, 63}"
+    options = r"\bg_black \color[RGB]{240, 240, 240} \pagecolor[RGB]{49, 51, 56}"
     # bg_black is for putting a black background (custom command of the site) instead of a transparent one
     # only then a custom background color can be used with pagecolor. color is for the text color
     url = f"https://latex.codecogs.com/png.latex?\\dpi{{200}} {options} {equation}"
