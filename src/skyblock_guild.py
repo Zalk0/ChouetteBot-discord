84e3a4e4db394fd5831aa152e37ca948e2b04436
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-10-19 22:30:29 +0200

    Migration from requests to aiohttp done

--- src/skyblock_guild.py
+++ src/skyblock_guild.py
@@ -1,41 +1,46 @@
-import requests
+import aiohttp
 from dotenv import dotenv_values
 
 api_hypixel = "https://api.hypixel.net/"
 token_hypixel = dotenv_values()['HYPIXEL_KEY']
 
 
-def return_discord_hypixel(uuid):
-    req = requests.get(f"{api_hypixel}player?key={token_hypixel}&uuid={uuid}").json()
+async def fetch(session, url, params=None):
+    async with session.get(url, params=params) as response:
+        return await response.json()
+
+
+async def return_discord_hypixel(session, uuid):
+    response = await fetch(session, f"{api_hypixel}player", {"key": token_hypixel, "uuid": uuid})
     try:
-        return req["player"]["socialMedia"]["links"]["DISCORD"]
+        return response["player"]["socialMedia"]["links"]["DISCORD"]
     except Exception:
-        if req["success"] == "true":
+        if response["success"] == "true":
             return 0
         return
 
 
-def return_uuid(pseudo):
-    req = requests.get(f"https://api.mojang.com/users/profiles/minecraft/{pseudo}").json()
+async def return_uuid(session, pseudo):
+    response = await fetch(session, f"https://api.mojang.com/users/profiles/minecraft/{pseudo}")
     try:
-        return req["id"]
+        return response["id"]
     except Exception:
         # This Minecraft pseudo doesn't exist
-        if req["errorMessage"] == f"Couldn't find any profile with name {pseudo}":
+        if response["errorMessage"] == f"Couldn't find any profile with name {pseudo}":
             return 0
         return
 
 
-def return_guild(name):
-    req = requests.get(f"{api_hypixel}guild?key={token_hypixel}&name={name}").json()
+async def return_guild(session, name):
+    response = await fetch(session, f"{api_hypixel}guild", {"key": token_hypixel, "name": name})
     try:
-        return req["guild"]
+        return response["guild"]
     except Exception:
         return
 
 
-def is_in_guild(uuid, g_name):
-    guild = return_guild(g_name)
+async def is_in_guild(session, uuid, g_name):
+    guild = await return_guild(session, g_name)
     if guild is None:
         return
     for member in guild["members"]:
@@ -44,24 +49,25 @@ def is_in_guild(uuid, g_name):
     return False
 
 
-def check(pseudo, guild, discord):
-    uuid = return_uuid(pseudo)
-    if uuid == 0:
-        return f"Il n'y a pas de compte Minecraft avec ce pseudo : {pseudo}"
-    elif uuid is None:
-        return "Something wrong happened"
+async def check(pseudo, guild, discord):
+    async with aiohttp.ClientSession() as session:
+        uuid = await return_uuid(session, pseudo)
+        if uuid == 0:
+            return f"Il n'y a pas de compte Minecraft avec ce pseudo : {pseudo}"
+        elif uuid is None:
+            return "Something wrong happened"
 
-    discord_mc = return_discord_hypixel(uuid)
-    if discord_mc == 0:
-        return "Vous n'avez pas entré de pseudo Discord sur le serveur Hypixel"
-    elif not discord_mc == discord:
-        return "Votre pseudo Discord ne correspond pas à celui entré sur le serveur Hypixel"
-    elif discord_mc is None:
-        return "Something wrong happened"
+        discord_mc = await return_discord_hypixel(session, uuid)
+        if discord_mc == 0:
+            return "Vous n'avez pas entré de pseudo Discord sur le serveur Hypixel"
+        elif not discord_mc == discord:
+            return "Votre pseudo Discord ne correspond pas à celui entré sur le serveur Hypixel"
+        elif discord_mc is None:
+            return "Something wrong happened"
 
-    test = is_in_guild(uuid, guild)
-    if test:
-        return True
-    elif test is None:
-        return f"Il n'y a pas de guilde avec ce nom : {guild}"
-    return "Something very wrong happened"
+        test = await is_in_guild(session, uuid, guild)
+        if test:
+            return True
+        elif test is None:
+            return f"Il n'y a pas de guilde avec ce nom : {guild}"
+        return "Something very wrong happened"
