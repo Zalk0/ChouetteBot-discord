12c2796d06921c280ee21b633e1601d50b0e59e6
Gylfirst <30391973+Gylfirst@users.noreply.github.com>
2023-11-27 23:02:30 +0100

    Update admin.py
    correction of keyboard error in the whisper command

--- commands/admin.py
+++ commands/admin.py
@@ -19,5 +19,5 @@ async def is_admin(interaction: discord.Interaction[ChouetteBot]):
 @app_commands.check(is_admin)
 @app_commands.command(name="whisper", description="Whisper an admin message")
 async def whisper(interaction: discord.Interaction[ChouetteBot], message: str):
-    await interaction.channel.send(f"{interaction.client.user.name} wants so say this message: {message}")
+    await interaction.channel.send(f"{interaction.client.user.name} wants to say this message: {message}")
     await interaction.response.send_message("Commande réussie", ephemeral=True, delete_after=2)
