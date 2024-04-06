716941aa1c84928c0b949129b25b5fa9162a1a03
Zalk0 <88582103+zalk0@users.noreply.github.com>
2024-04-06 15:28:56 +0200

    chore: add type hints

--- utils/birthdays.py
+++ utils/birthdays.py
@@ -4,26 +4,26 @@ from pathlib import Path
 import tomlkit
 
 Path("data").mkdir(exist_ok=True)
-birthday_file_path = Path("data", "birthdays.toml")
+BIRTHDAY_FILE = Path("data", "birthdays.toml")
 
 
 # Charge les anniversaires depuis un fichier TOML
-def load_birthdays():
+def load_birthdays() -> dict:
     try:
-        with open(birthday_file_path) as f:
+        with open(BIRTHDAY_FILE) as f:
             return tomlkit.load(f)
     except FileNotFoundError:
         return {}
 
 
 # Enregistre les anniversaires dans le fichier TOML
-def save_birthdays(birthdays):
-    with open(birthday_file_path, "w") as f:
+def save_birthdays(birthdays: dict):
+    with open(BIRTHDAY_FILE, "w") as f:
         tomlkit.dump(birthdays, f)
 
 
 # Permet de vérifier si la valeur est correcte
-async def check_year_value(year):
+async def check_year_value(year: int) -> int:
     if not year:
         return 1
     if year < 1900 or year > date.today().year:
@@ -32,5 +32,5 @@ async def check_year_value(year):
 
 
 # Permet de calculer l'âge
-async def calculate_age(year: int):
+async def calculate_age(year: int) -> int:
     return date.today().year - year if year != 1 else None
