aa234a5c615e325a30bf3b5fbb8968a2de952ba8
Zalk0 <88582103+zalk0@users.noreply.github.com>
2026-05-31 11:18:43 +0200

    feat(sb): new Minecraft versions for mods cmd

--- chouette/commands/skyblock.py
+++ chouette/commands/skyblock.py
@@ -31,73 +31,46 @@ class Skyblock(app_commands.Group):
     async def mods(
         self,
         interaction: discord.Interaction[ChouetteBot],
-        mc_version: Literal["1.8.9", "1.21.1", "1.21.5"],
+        mc_version: Literal["1.21.10", "1.21.11", "26.1.2"],
     ) -> None:
         """Vérifie les dernières mises à jour des mods populaires du Skyblock d'Hypixel.
 
         Args:
             interaction (discord.Interaction[ChouetteBot]): L'interaction Discord.
-            mc_version (Literal["1.8.9", "1.21.1", "1.21.5"]): La version de Minecraft.
+            mc_version (Literal): La version de Minecraft.
         """
         await interaction.response.defer(thinking=True)
         message = f"Version de Minecraft: `{mc_version}`\n"
-        api_github = "https://api.github.com/repos"
         api_modrinth = "https://api.modrinth.com/v2"
         session = interaction.client.session
 
-        if mc_version == "1.8.9":
-            # Mod loader: Forge
-            async with session.get(
-                f"{api_github}/Dungeons-Guide/Skyblock-Dungeons-Guide/releases/latest"
-            ) as response:
-                dungeonsguide = await response.json()
-            async with session.get(f"{api_modrinth}/project/GGamhqbw/version") as response:
-                notenoughupdates = (await response.json())[0]
-            async with session.get(f"{api_modrinth}/project/F35D4vTL/version") as response:
-                skyblockaddons = (await response.json())[0]
-            async with session.get(f"{api_github}/Skytils/SkytilsMod/releases/latest") as response:
-                skytils = await response.json()
+        # Certains mods ne sont disponibles que pour Fabric, mais ils pourraient
+        # être disponibles pour d'autres loaders à l'avenir (ex: NeoForge).
+        message += (
+            "Mod loader: `Fabric`\n"
+            "--------------------------------\n"
+            "Les dernières mises à jour sont :\n"
+        )
+        mod_list = {
+            "Aaron's Mod": "axe0DxiW",
+            "Catharsis": "fc4wBpRx",
+            "Firmament": "IJNUBZ2a",
+            "Roughly Enough Items (REI)": "nfn13YXA",
+            "Skyblocker": "y6DuFGwJ",
+        }
 
-            message += (
-                "Mod loader: `Forge`\n"
-                "--------------------------------\n"
-                "Les dernières mises à jour sont :\n"
-                f"- Dungeons-Guide: `{dungeonsguide['tag_name'].replace('v', '')}` "
-                f"[lien]({dungeonsguide['assets'][0]['browser_download_url']})\n"
-                f"- NotEnoughUpdates: `{notenoughupdates['version_number']}` "
-                f"[lien]({notenoughupdates['files'][0]['url']})\n"
-                f"- SkyblockAddons (forked by Fix3dll): `{skyblockaddons['version_number']}` "
-                f"[lien]({skyblockaddons['files'][0]['url']})\n"
-                f"- Skytils: `{skytils['tag_name'].replace('v', '')}` "
-                f"[lien]({skytils['assets'][0]['browser_download_url']})"
-            )
-
-        elif mc_version == "1.21.1" or mc_version == "1.21.5":
-            # Mod loader: Fabric
-            # Certains mods ne sont disponibles que pour Fabric, mais ils pourraient
-            # être disponibles pour d'autres loaders à l'avenir (ex: NeoForge).
-            message += (
-                "Mod loader: `Fabric`\n"
-                "--------------------------------\n"
-                "Les dernières mises à jour sont :\n"
-            )
-            mod_list = {
-                "Aaron's Mod": "axe0DxiW",
-                "Firmament": "IJNUBZ2a",
-                "Roughly Enough Items (REI)": "nfn13YXA",
-                "Skyblocker": "y6DuFGwJ",
-            }
-
-            for mod_name, mod_id in mod_list.items():
-                async with session.get(f"{api_modrinth}/project/{mod_id}/version") as response:
-                    for entry in await response.json():
-                        if mc_version in entry.get("game_versions", []) and "fabric" in entry.get(
-                            "loaders", []
-                        ):
-                            version = entry["version_number"].split("+")[0].replace("v", "")
-                            link = entry["files"][0]["url"]
-                            message += f"- {mod_name}: `{version}` [lien]({link})\n"
-                            break
+        for mod_name, mod_id in mod_list.items():
+            async with session.get(f"{api_modrinth}/project/{mod_id}/version") as response:
+                for entry in await response.json():
+                    if mc_version in entry.get("game_versions", []) and "fabric" in entry.get(
+                        "loaders", []
+                    ):
+                        version = entry["version_number"].split("+")[0].replace("v", "")
+                        link = entry["files"][0]["url"]
+                        message += f"- {mod_name}: `{version}` [lien]({link})\n"
+                        break
+                else:
+                    message += f"- {mod_name}: Non disponible\n"
 
         await interaction.followup.send(message)
 
