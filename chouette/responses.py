7d68e588bab3a6f448ce47c5f0d562d71d0a743a
Zalk0 <88582103+zalk0@users.noreply.github.com>
2026-09-19 11:57:03 +0200

    fix: enable back some ignored rules

--- chouette/responses.py
+++ chouette/responses.py
@@ -38,10 +38,8 @@ async def responses(
         return "**FEUR**", False
 
     # Checks if a message contains $$ to signify LaTeX expression
-    if message.count("$") > 1:
+    if message.count("$") > 1 and not (message.count("$") == 2 and "$$" in message):
         if (message.count("$") % 2) == 0:
-            if message.count("$") == 2 and "$$" in message:
-                return "", False
             await channel.send(file=await latex_process(client.session, message))
             client.bot_logger.info(f'{client.user} responded to {author}: "equation.png"')
             return "", False
